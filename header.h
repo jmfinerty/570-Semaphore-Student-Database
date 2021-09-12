d672b2e24a2177c0006878e1def94972701081bb
jmfinerty <jmfinerty@gmail.com>
2021-09-12 14:14:39 -0400

    change placeholder student id

--- header.h
+++ header.h
@@ -1,10 +1,10 @@
 /* header.h */
 
-#define KEY ((key_t)(124345)) // change it to last five digits of your SID
+#define KEY ((key_t)(07438)) // changed to last five digits of my student ID
 #define SEGSIZE	sizeof(struct StudentInfo)
 
 #define NUM_SEMAPHS 5
-#define SEMA_KEY ((key_t)(44456)) // change this to last four digits of SID
+#define SEMA_KEY ((key_t)(7438)) // changed this to last four digits of my student ID
 
 struct StudentInfo{
 	char fName[20];
