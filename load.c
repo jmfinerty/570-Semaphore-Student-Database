8b42d5b8862a366b191fe3123982beec01b8f121
jmfinerty <jmfinerty@gmail.com>
2021-09-12 19:19:49 -0400

    trim newlines from info, and ignore newline at EOF

--- load.c
+++ load.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
@@ -38,15 +39,23 @@ int main(int argc, char *argv[]) {
         *read_count = 0;
 
         while (!feof(f)) {
-            fgets(student->Name, 60, f);
-            fgets(student->StuID, 18, f);
-            fgets(student->Address, 100, f);
-            fgets(student->Phone, 20, f);
+            fgets(student->Name, 51, f);
+            if ((int)strlen(student->Name) == 0)
+                break;
+            fgets(student->StuID, 26, f);
+            fgets(student->Address, 76, f);
+            fgets(student->Phone, 26, f);
+
+            // remove newlines
+            student->Name[strlen(student->Name) - 1] = '\0';
+            student->StuID[strlen(student->StuID) - 1] = '\0';
+            student->Address[strlen(student->Address) - 1] = '\0';
+            student->Phone[strlen(student->Phone) - 1] = '\0';
+
             printf("Loaded: %s\n", student->Name);
             student++;
         }
 
-        sleep(2);
         Signal(semaset, 0);
 
     } else {
