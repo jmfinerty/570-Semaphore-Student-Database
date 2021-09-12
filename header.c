be1edb3adee1ed32d4df05f13e0ab1a0a808fa20
jmfinerty <jmfinerty@gmail.com>
2021-09-12 14:12:51 -0400

    an attempt at beautification

--- header.c
+++ header.c
@@ -1,4 +1,4 @@
-/*header.c*/
+/* header.c */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,37 +8,33 @@
 #include <sys/sem.h>
 #include "header.h"
 
-/* Lock the semaphore n of the semaphore set semaph */
-
-void Wait(int semaph, int n)
-{
-  struct sembuf sop;
-  sop.sem_num = n;
-  sop.sem_op =  -1;
-  sop.sem_flg = 0;
-  semop(semaph,&sop,1);
+// Lock the semaphore n of the semaphore set semaph
+void Wait(int semaph, int n) {
+	struct sembuf sop;
+	sop.sem_num = n;
+	sop.sem_op = -1;
+	sop.sem_flg = 0;
+	semop(semaph, &sop, 1);
 }
 
-/* Unlock the semaphore n of the semaphore set semaph */
-void Signal(int semaph, int n)
-{
-  struct sembuf sop;
-  sop.sem_num = n;
-  sop.sem_op =  1;
-  sop.sem_flg = 0;
-  semop(semaph,&sop,1);
+// Unlock the semaphore n of the semaphore set semaph
+void Signal(int semaph, int n) {
+	struct sembuf sop;
+	sop.sem_num = n;
+	sop.sem_op = 1;
+	sop.sem_flg = 0;
+	semop(semaph, &sop, 1);
 }
 
-/* make an array of n semaphores with key k */
-int GetSemaphs(key_t k, int n)
-{
-  int semid, i;
+// make an array of n semaphores with key k
+int GetSemaphs(key_t k, int n) {
+	int semid, i;
 
-  /* get a set of n semaphores with the given key k */
-  
-  if ((semid=semget(k,n,IPC_CREAT|0666))!= -1){
-    for (i=0;i<n;i++)
-      Signal(semid,i); /* unlock all the semaphores */
-  }
-  return semid;
+	// get a set of n semaphores with the given key k
+	if ((semid = semget(k, n, IPC_CREAT|0666)) != -1) {
+		for (i = 0; i < n; i++) {
+			Signal(semid, i); // unlock all the semaphores
+    }
+	}
+	return semid;
 }
--- header.h
+++ header.h
@@ -1,16 +1,16 @@
-
 /* header.h */
-#define KEY  ((key_t)(124345)) /*change it to last five digits of your SID*/
-#define SEGSIZE  sizeof(struct StudentInfo)
+
+#define KEY ((key_t)(124345)) // change it to last five digits of your SID
+#define SEGSIZE	sizeof(struct StudentInfo)
 
 #define NUM_SEMAPHS 5
-#define SEMA_KEY   ((key_t)(44456)) /* change this to last four digits of SID */
+#define SEMA_KEY ((key_t)(44456)) // change this to last four digits of SID
 
 struct StudentInfo{
-  char fName[20];
-  char lName[20];
-  char telNumber[15];
-  char whoModified[10];
+	char fName[20];
+	char lName[20];
+	char telNumber[15];
+	char whoModified[10];
 };
 
 void Wait(int semaph, int n);
